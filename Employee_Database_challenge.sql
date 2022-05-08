b72042603ad4219b369a90a9327274beac1abaeb
NRalston <Neil.Ralston@MSPMAC.ORG>
2022-05-08 13:50:43 -0500

    upload challenge update

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+--DELIVERABLE 1
 SELECT employees.emp_no,
 	employees.first_name,
 	employees.last_name,
@@ -10,7 +11,6 @@ LEFT JOIN titles
 ON employees.emp_no = titles.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
-
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (retirement_info_title.emp_no) retirement_info_title.emp_no,
@@ -28,8 +28,19 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT (title) DESC;
 
-
-
-
-
-
+--DELIVERABLE 2
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	dept_employees.from_date,
+	dept_employees.to_date,
+	titles.title
+INTO mentor_elig
+FROM employees
+LEFT JOIN dept_employees
+ON employees.emp_no = dept_employees.emp_no
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE dept_employees.to_date = ('9999-01-01')
+AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
